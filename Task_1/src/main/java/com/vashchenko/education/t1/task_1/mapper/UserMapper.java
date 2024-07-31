a6da499993949afe893a32ca74df59a321070440
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-31 21:52:43 +0300

    Task_1: fix!: исправление бага генерации мапперов

--- Task_1/src/main/java/com/vashchenko/education/t1/task_1/mapper/UserMapper.java
+++ Task_1/src/main/java/com/vashchenko/education/t1/task_1/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.vashchenko.education.t1.task_1.mapper;
 
 import com.vashchenko.education.t1.task_1.model.dto.request.UserDto;
 import com.vashchenko.education.t1.task_1.model.entity.User;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import java.util.Collection;
 import java.util.List;
@@ -11,14 +10,11 @@ import java.util.List;
 public interface UserMapper extends BaseMapper<User, UserDto> {
 
     @Override
-    @BeanMapping(ignoreByDefault = false)
     User toEntity(UserDto to);
 
     @Override
-    @BeanMapping(ignoreByDefault = false)
     UserDto toDto(User entity);
 
     @Override
-    @BeanMapping(ignoreByDefault = false)
     List<UserDto> toDtoList(Collection<User> users);
 }
