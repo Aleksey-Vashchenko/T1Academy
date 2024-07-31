e7a05b4d70b50ea2fa7eebd76cedbee34ec0a8d4
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-07-31 13:48:44 +0300

    Task_1: fix: изменение названия поля UserDto.java

--- Task_1/src/main/java/com/vashchenko/education/t1/task_1/model/dto/request/UserDto.java
+++ Task_1/src/main/java/com/vashchenko/education/t1/task_1/model/dto/request/UserDto.java
@@ -2,5 +2,5 @@ package com.vashchenko.education.t1.task_1.model.dto.request;
 
 import java.util.UUID;
 
-public record UserDto(UUID id, String username, String email) {
+public record UserDto(UUID id, String email, String name) {
 }
