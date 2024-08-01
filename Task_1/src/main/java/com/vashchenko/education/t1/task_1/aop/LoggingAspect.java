8244440fc85e1ba2d69b80520e9db75f7801516a
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-08-01 16:41:46 +0300

    Task_1: feat: добавления логирования Log4j2: добавление зависимостей, файла конфигурации log4j2.xml с выводом всех логов в консоль и в файл, и с дополнительным файлом логов для логирования через аннотации

--- Task_1/src/main/java/com/vashchenko/education/t1/task_1/aop/LoggingAspect.java
+++ Task_1/src/main/java/com/vashchenko/education/t1/task_1/aop/LoggingAspect.java
@@ -1,21 +1,20 @@
 package com.vashchenko.education.t1.task_1.aop;
 
 import com.vashchenko.education.t1.task_1.exception.BaseConflictException;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.ThreadContext;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Log4j2
 public class LoggingAspect {
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
-
     @Pointcut("@annotation(com.vashchenko.education.t1.task_1.aop.annotation.LogMethodExecution)")
     public void needLoggingMethod() {}
 
@@ -25,16 +24,15 @@ public class LoggingAspect {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
-
-        logger.info("Method {}.{} called with parameters: {}", className, methodName, args);
+        log.info("Method {}.{} called with parameters: {}", className, methodName, args);
 
         try {
             Object result = joinPoint.proceed();
-            logger.info("Method {}.{} returned with result: {}", className, methodName, result);
+            log.info("Method {}.{} returned with result: {}", className, methodName, result);
             return result;
         } catch (Exception ex) {
             if(isNeedToLogin(ex)){
-                logger.error("Exception in method {}.{} with arguments {}. Exception: {}",
+                log.error("Exception in method {}.{} with arguments {}. Exception: {}",
                         className, methodName, args, ex.getMessage(), ex);
             }
             throw ex;
@@ -50,7 +48,7 @@ public class LoggingAspect {
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
         if(isNeedToLogin(ex)){
-            logger.error("Exception in method {}.{} with arguments {}. Exception: {}",
+            log.error("Exception in method {}.{} with arguments {}. Exception: {}",
                     className, methodName, args, ex.getMessage(), ex);
         }
     }
