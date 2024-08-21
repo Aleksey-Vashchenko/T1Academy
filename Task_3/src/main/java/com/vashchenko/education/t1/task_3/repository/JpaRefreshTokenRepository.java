b56ac341a8c77151e3e08b2bc21bee141ccb5f7e
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-08-21 15:56:38 +0300

    Task_3: init: создание реопзиторием для работы с сущностями

--- Task_3/src/main/java/com/vashchenko/education/t1/task_3/repository/JpaRefreshTokenRepository.java
+++ Task_3/src/main/java/com/vashchenko/education/t1/task_3/repository/JpaRefreshTokenRepository.java
@@ -4,8 +4,10 @@ import com.vashchenko.education.t1.task_3.model.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface JpaRefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByTokenValue(String tokenValue);
 }
--- Task_3/src/main/java/com/vashchenko/education/t1/task_3/repository/JpaUserRepository.java
+++ Task_3/src/main/java/com/vashchenko/education/t1/task_3/repository/JpaUserRepository.java
@@ -1,7 +1,10 @@
 package com.vashchenko.education.t1.task_3.repository;
 import com.vashchenko.education.t1.task_3.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaUserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
