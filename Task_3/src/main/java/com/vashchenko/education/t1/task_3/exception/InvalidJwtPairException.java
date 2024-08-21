d318650b9dc87f304a74cdf51660b147e6e20ff9
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-08-21 16:59:08 +0300

    Task_3: fix:изменение типа ошибка InvalidJwtPairException.java

--- Task_3/src/main/java/com/vashchenko/education/t1/task_3/exception/InvalidJwtPairException.java
+++ Task_3/src/main/java/com/vashchenko/education/t1/task_3/exception/InvalidJwtPairException.java
@@ -1,6 +1,8 @@
 package com.vashchenko.education.t1.task_3.exception;
 
-public class InvalidJwtPairException extends RuntimeException{
+import io.jsonwebtoken.JwtException;
+
+public class InvalidJwtPairException extends JwtException {
     public InvalidJwtPairException() {
         super("Invalid jwt pair");
     }
