7d6d66b4ae79c90c76ff5c5a02cdc47280faf2cb
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-08-22 16:26:37 +0300

    Task_3: feat: добавление свойств приложения и создание пользователя при запуске

--- Task_3/src/main/java/com/vashchenko/education/t1/task_3/Task3Application.java
+++ Task_3/src/main/java/com/vashchenko/education/t1/task_3/Task3Application.java
@@ -1,13 +1,29 @@
 package com.vashchenko.education.t1.task_3;
 
+import com.vashchenko.education.t1.task_3.service.UserService;
+import com.vashchenko.education.t1.task_3.web.dto.request.RegistrationRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(securedEnabled = true,prePostEnabled = true)
+@RequiredArgsConstructor
 public class Task3Application {
-
+    private final UserService userService;
     public static void main(String[] args) {
         SpringApplication.run(Task3Application.class, args);
+    }
+
+    @Profile("!test")
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationReady(){
+        RegistrationRequestDto requestDto = new RegistrationRequestDto("123","123","123");
+        userService.registration(requestDto);
     }
 
 }
